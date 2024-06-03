bc63d63d2116de535e6d66eb630afa77f76be862
Soundarrajan Thiruvenkatasamy <soundarrajan.official@gmail.com>
2024-06-03 14:07:55 +0530

    Connecting and accessing the player name input element using useRef React Hook

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,26 +1,20 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 export default function Player() {
+    let playerNameRef = useRef(null)
     const [enteredPlayerName, setEnteredPlayerName] = useState("")
-    const [isSubmitted, setIsSubmitted] = useState(false)
-
-    const nameHandler = (event) => {
-        setIsSubmitted(false)
-        setEnteredPlayerName(event.target.value)
-    }
 
     const clickHandler = () => {
-        setIsSubmitted(true)
+        setEnteredPlayerName(playerNameRef.current.value)
     }
 
   return (
     <section id="player">
-      <h2>Welcome {isSubmitted ? enteredPlayerName : "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}</h2>
       <p>
         <input
+            ref={playerNameRef}
             type="text"
-            value={enteredPlayerName}
-            onChange={nameHandler}
         />
         <button onClick={clickHandler}>Set Name</button>
       </p>
