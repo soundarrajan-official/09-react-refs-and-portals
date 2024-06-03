ac9f4d12e61693f4762b1ff2de83b2123ea38705
Soundarrajan Thiruvenkatasamy <soundarrajan.official@gmail.com>
2024-06-03 12:34:06 +0530

    Add state for player name and submit button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,28 @@
+import {useState} from "react";
+
 export default function Player() {
+    const [enteredPlayerName, setEnteredPlayerName] = useState("")
+    const [isSubmitted, setIsSubmitted] = useState(false)
+
+    const nameHandler = (event) => {
+        setIsSubmitted(false)
+        setEnteredPlayerName(event.target.value)
+    }
+
+    const clickHandler = () => {
+        setIsSubmitted(true)
+    }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {isSubmitted ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+            type="text"
+            value={enteredPlayerName}
+            onChange={nameHandler}
+        />
+        <button onClick={clickHandler}>Set Name</button>
       </p>
     </section>
   );
