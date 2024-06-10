da431a54275a559f9eca89f17a4798f53c55a7d5
Soundarrajan Thiruvenkatasamy <soundarrajan.official@gmail.com>
2024-06-10 11:31:12 +0530

    Use refs to set timeout and clear timeout

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,15 +1,24 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
+
+// let timer
 
 const TimerChallenge = ({title, targetTime}) => {
+    const timer = useRef()
     const [timerExpired, setTimerExpired] = useState(false)
     const [timerStarted, setTimerStarted] = useState(false)
 
+    // let timer;
+
     const handleStart = () => {
-        setTimeout(() => {
+        timer.current = setTimeout(() => {
             setTimerExpired(true)
-        }, targetTime * 1000)
+        }, targetTime * 1000);
 
         setTimerStarted(true)
+    }
+
+    const handleStop = () => {
+        clearTimeout(timer.current)
     }
 
     return (
@@ -20,7 +29,7 @@ const TimerChallenge = ({title, targetTime}) => {
                 {targetTime} second{targetTime > 1 ?? "s"}
             </p>
             <p>
-                <button onClick={handleStart}>
+                <button onClick={timerStarted ? handleStop : handleStart}>
                     {timerStarted ? 'Stop':  'Start'} Challenge
                 </button>
             </p>
